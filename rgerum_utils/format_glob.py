40ae3b21f35f3e6c36f19cc67055f3edcfe4fde9
rgerum <14153051+rgerum@users.noreply.github.com>
2023-03-28 11:15:03 -0400

    added path_not_found_message

--- rgerum_utils/format_glob.py
+++ rgerum_utils/format_glob.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pathlib import Path
+import glob
+import sys
 
 
 def format_glob(pattern, return_template=False):
@@ -60,6 +62,63 @@ def format_glob_pd(pattern, return_template=False):
     return pd.DataFrame(data)
 
 
+def path_not_found_message(name):
+    def levenshteinDistance(str1, str2):
+        m = len(str1)
+        n = len(str2)
+        d = [[i] for i in range(1, m + 1)]  # d matrix rows
+        d.insert(0, list(range(0, n + 1)))  # d matrix columns
+        for j in range(1, n + 1):
+            for i in range(1, m + 1):
+                if str1[i - 1] == str2[j - 1]:  # Python (string) is 0-based
+                    substitutionCost = 0
+                else:
+                    substitutionCost = 1
+                d[i].insert(j, min(d[i - 1][j] + 1,
+                                   d[i][j - 1] + 1,
+                                   d[i - 1][j - 1] + substitutionCost))
+        return d[-1][-1]
+
+    # get a list of all parent folders
+    name = Path(name).absolute()
+    hierarchy = []
+    while name.parent != name:
+        hierarchy.append(name)
+        name = name.parent
+
+    parent_folder_count = 1
+    # iterate over the parent folders, starting from the lowest
+    for path in hierarchy[::-1]:
+        # check if the path exists (or is a glob pattern with matches)
+        if "*" in str(path):
+            exists = len(glob.glob(str(path)))
+        else:
+            exists = path.exists()
+
+        # if it does not exist, we have found our problem
+        if not exists:
+            similar_paths = list([p.name for p in path.parent.glob("*")])
+            distances = [levenshteinDistance(p, str(path.name)) for p in similar_paths]
+            import numpy as np
+            i = np.argmin(distances)
+
+            target = f"no file/folder \"{path.name}\" found"
+            if "*" in str(path.name):
+                target = f"pattern \"{path.name}\" not found"
+            source = f"in folder \"{path.parent}\""
+            if "*" in str(path.parent):
+                if parent_folder_count == 1:
+                    source = f"in the only folder matching the pattern \"{path.parent}\""
+                else:
+                    source = f"in any of the {parent_folder_count} folders matching the pattern \"{path.parent}\""
+            if distances[i] < 10:
+                print(f"WARNING: {source} {target}. Did you mean \"{similar_paths[i]}\"?", file=sys.stderr)
+            else:
+                print(f"WARNING: {source} {target}", file=sys.stderr)
+            break
+        parent_folder_count = exists
+
+
 if __name__ == "__main__":
     from mock_dir import MockDir
     # to test we create an artificial folder structure
@@ -67,6 +126,8 @@ if __name__ == "__main__":
         "tmp": {
             "run-1": ["run_nodes3_name-Alice.txt", "run_nodes4_name-Bob.txt", "run_nodes7_name-Foo.txt", "run_nodes7.8_name-Bar.txt"],
             "run-2": ["run_nodes3_name-Alice.txt", "run_nodes4_name-Bob.txt", "run_nodes7_name-Foo.txt", "run_nodes7.8_name-Bar.txt"],
+            "running-2": ["run_nodes3_name-Alice.txt", "run_nodes4_name-Bob.txt", "run_nodes7_name-Foo.txt", "run_nodes7.8_name-Bar.txt"],
+            "boo-2": ["run_nodes3_name-Alice.txt", "run_nodes4_name-Bob.txt", "run_nodes7_name-Foo.txt", "run_nodes7.8_name-Bar.txt"],
         }
     }
     with MockDir(file_structure):
